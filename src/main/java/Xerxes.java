2d62f48d930e3ab3d489b25e88c1889848294fc0
Zheng Yong <36950067+ZYS-37@users.noreply.github.com>
2026-08-19 17:57:56 +0800

    add greeting

--- src/main/java/Xerxes.java
+++ src/main/java/Xerxes.java
@@ -5,6 +5,15 @@ public class Xerxes {
                 + " \\  /  / _ \\| '__|\\ \\/ / / _ \\/ __|\n"
                 + " /  \\ |  __/| |    >  < |  __/\\__ \\\n"
                 + "/_/\\_\\ \\___||_|   /_/\\_\\ \\___||___/\n";
+        String divider = "____________________________________________________________";
+        String greeting = "Yo wassup! I'm Xerxes.\n" +
+                "What do ya need?\n";
+        String exit = "Ciao, cya again!\n";
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(divider);
+        System.out.println(exit);
+        System.out.println(divider);
     }
 }
