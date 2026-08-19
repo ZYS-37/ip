0ddd6c254cc93f8022bcc8fe1dca62499e41d1b0
Zheng Yong <36950067+ZYS-37@users.noreply.github.com>
2026-08-19 22:09:03 +0800

    add task list functionality

--- src/main/java/Xerxes.java
+++ src/main/java/Xerxes.java
@@ -1,24 +1,30 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Xerxes {
     public static final String DIVIDER = "____________________________________________________________";
 
-    public static final String BANNER = " __  __                        \n"
+    public static final String BANNER = "__  __                        \n"
             + "\\ \\/ /  ___   _ __ __  __  ___  ___ \n"
             + " \\  /  / _ \\| '__|\\ \\/ / / _ \\/ __|\n"
             + " /  \\ |  __/| |    >  < |  __/\\__ \\\n"
             + "/_/\\_\\ \\___||_|   /_/\\_\\ \\___||___/";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Task> tasks = new ArrayList<>();
         initialMsg();
-        boolean echo = true;
-        while (echo) {
+        boolean active = true;
+        while (active) {
             String userMsg = scanner.nextLine();
-            if (!Objects.equals(userMsg, "bye")) {
-                msgWithDivider(userMsg);
+            if (Objects.equals(userMsg, "bye")) {
+                active = false;
+            } else if (Objects.equals(userMsg, "list")) {
+                printTaskList(tasks);
             } else {
-                echo = false;
+                tasks.add(new Task(userMsg));
+                msgWithDivider(userMsg);
             }
         }
         msgWithDivider("Ciao, cya again");
@@ -36,5 +42,14 @@ public class Xerxes {
         printDivider();
         System.out.println(BANNER);
         msgWithDivider("Yo wassup! I'm Xerxes.\nWhat do ya need?");
+    }
+
+    public static void printTaskList(List<Task> tasks) {
+        int i = 1;
+        for (Task task : tasks) {
+            System.out.println((i) + ": " + task);
+            i++;
+        }
+        printDivider();
     }
 }
