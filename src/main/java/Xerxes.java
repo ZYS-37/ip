adcc594591644e08667e4cfcba5c2f566a1ac421
Zheng Yong <36950067+ZYS-37@users.noreply.github.com>
2026-08-19 20:41:08 +0800

    refactor Xerxes.java to be modular

--- src/main/java/Xerxes.java
+++ src/main/java/Xerxes.java
@@ -1,19 +1,28 @@
 public class Xerxes {
+    public static final String DIVIDER = "____________________________________________________________";
+
+    public static final String BANNER = " __  __                        \n"
+            + "\\ \\/ /  ___   _ __ __  __  ___  ___ \n"
+            + " \\  /  / _ \\| '__|\\ \\/ / / _ \\/ __|\n"
+            + " /  \\ |  __/| |    >  < |  __/\\__ \\\n"
+            + "/_/\\_\\ \\___||_|   /_/\\_\\ \\___||___/";
     public static void main(String[] args) {
-        String banner = " __  __                        \n"
-                + "\\ \\/ /  ___   _ __ __  __  ___  ___ \n"
-                + " \\  /  / _ \\| '__|\\ \\/ / / _ \\/ __|\n"
-                + " /  \\ |  __/| |    >  < |  __/\\__ \\\n"
-                + "/_/\\_\\ \\___||_|   /_/\\_\\ \\___||___/\n";
-        String divider = "____________________________________________________________";
-        String greeting = "Yo wassup! I'm Xerxes.\n" +
-                "What do ya need?\n";
-        String exit = "Ciao, cya again!\n";
-        System.out.println(divider);
-        System.out.println(banner);
-        System.out.println(greeting);
-        System.out.println(divider);
-        System.out.println(exit);
-        System.out.println(divider);
+
+        initialMsg();
+        msgWithDivider("Ciao, cya again");
+    }
+
+    public static void printDivider() {
+        System.out.println(DIVIDER);
+    }
+    public static (String msg) {
+        System.out.println(msg);
+        printDivider();
+    }
+
+    public static void initialMsg() {
+        printDivider();
+        System.out.println(BANNER);
+        msgWithDivider("Yo wassup! I'm Xerxes.\nWhat do ya need?");
     }
 }
