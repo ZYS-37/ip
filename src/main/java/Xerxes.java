c1e23c9a51ad883250129ef6c61f8f08f5539197
Zheng Yong <36950067+ZYS-37@users.noreply.github.com>
2026-08-19 17:51:26 +0800

    Rename Duke Chatbot to Xerxes

---
+++ src/main/java/Xerxes.java
@@ -0,0 +1,10 @@
+public class Xerxes {
+    public static void main(String[] args) {
+        String banner = " __  __                        \n"
+                + "\\ \\/ /  ___   _ __ __  __  ___  ___ \n"
+                + " \\  /  / _ \\| '__|\\ \\/ / / _ \\/ __|\n"
+                + " /  \\ |  __/| |    >  < |  __/\\__ \\\n"
+                + "/_/\\_\\ \\___||_|   /_/\\_\\ \\___||___/\n";
+        System.out.println(banner);
+    }
+}
