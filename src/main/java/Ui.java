ba09d1addae240164498ac29f14a4e2d148b2f02
Zheng Yong <36950067+ZYS-37@users.noreply.github.com>
2026-08-24 18:58:51 +0800

    Implement Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Ui {
+    private static final String DIVIDER =
+            "____________________________________________________________";
+    public static final String BANNER = "__  __                        \n"
+            + "\\ \\/ /  ___   _ __ __  __  ___  ___ \n"
+            + " \\  /  / _ \\| '__|\\ \\/ / / _ \\/ __|\n"
+            + " /  \\ |  __/| |    >  < |  __/\\__ \\\n"
+            + "/_/\\_\\ \\___||_|   /_/\\_\\ \\___||___/";
+
+    private final Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showInitialMessage() {
+        printDivider();
+        System.out.println(BANNER);
+        showMessage("Yo wassup! I'm Xerxes.\nWhat do ya need?");
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+        printDivider();
+    }
+
+    public void showError(String message) {
+        showMessage(message);
+    }
+
+    public void printDivider() {
+        System.out.println(DIVIDER);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
