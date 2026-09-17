433de5e72136c7ffba5aa5458dd7c51b5046b774
Zheng Yong <36950067+ZYS-37@users.noreply.github.com>
2026-09-17 23:01:20 +0800

    Add terminal closure with bye

--- src/main/java/xerxes/MainWindow.java
+++ src/main/java/xerxes/MainWindow.java
@@ -1,5 +1,6 @@
 package xerxes;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -64,8 +65,7 @@ public class MainWindow extends AnchorPane {
         userInput.clear();
 
         if (result.shouldExit()) {
-            userInput.setDisable(true);
-            sendButton.setDisable(true);
+            Platform.exit();
         }
     }
 }
